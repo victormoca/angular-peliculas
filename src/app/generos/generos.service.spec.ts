5dd594ca51b46e866c8c3016157d5d38500a5771
victormoca <86364382+victormoca@users.noreply.github.com>
2025-07-05 20:44:04 -0600

    feat: add http client provider and fix tests

--- src/app/generos/generos.service.spec.ts
+++ src/app/generos/generos.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { GenerosService } from './generos.service';
 
@@ -6,7 +7,9 @@ describe('GenerosService', () => {
   let service: GenerosService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(GenerosService);
   });
 
